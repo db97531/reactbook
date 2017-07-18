62907f3e1bc712fa5ad975d84b9567f41152005e
db97531 <db97531@gmail.com>
2017-07-19 00:14:46 +0900

    Pause

--- js/app.js
+++ js/app.js
@@ -1,40 +1,102 @@
-var TextAreaCounter = React.createClass({
-  PropTypes: {
-    text: React.PropTypes.string,
-  },
+var headers = [
+  "タイトル", "著者", "言語", "出版年", "売上部数"
+];
 
-  getDefaultProps: function() {
-    return {
-      text: '',
-    }
+var data = [
+  ["The Lord of the Rings", "J. R. R. Tolkien",
+    "English", "1954-1955", "150 million"],
+  ["Le Petit Rrince (The Little Prince)", "Antoine de Saint-Exupery",
+    "French", "1943", "140 million"],
+  ["Harrya Potter and the Philosopher's Stone", "J. K. Rowling",
+    "English", "1997", "107 million"],
+  ["And Then There Were None", "Agatha Christie",
+    "English", "1939", "100 million"],
+  ["Dream of the Red Chanber", "Cao Xueqin",
+    "Chinese", "1754-1791", "100 million"],
+  ["The hobbit", "J. R. R. Tolkien",
+    "English", "1937", "100 million"],
+  ["She: A History of Adventure", "H. Rider Haggard",
+    "English", "1887", "100 million"],
+];
+
+var Excel = React.createClass({
+  displayName: 'Excel',
+
+  propTypes: {
+    headers: React.PropTypes.arrayOf(
+      React.PropTypes.string
+    ),
+    initialData: React.PropTypes.arrayOf(
+      React.PropTypes.arrayOf(
+        React.PropTypes.string
+      )
+    ),
   },
 
   getInitialState: function() {
     return {
-      text: this.props.text,
+      data: this.props.initialData,
+      sortby: null,
+      descending: false,
     };
   },
 
-  _textChange: function(ev) {
+  _sort: function(e) {
+
+    var column = e.target.cellIndex;
+    var descending = this.state.sortby === column && !this.state.descending;
+    var data = this.state.data.slice();
+    data.sort(function(a, b) {
+        return descending
+            ? (a[column] < b[column] ? 1 : -1)
+            : (a[column] > b[column] ? 1 : -1);
+    });
     this.setState({
-      text: ev.target.value,
+      data: data,
+      sortby: column,
+      descending: descending,
     });
   },
 
   render: function() {
-    return React.DOM.div(null,
-      React.DOM.textarea({
-        value: this.state.text,
-        onChange: this._textChange,
-      }),
-      React.DOM.h3(null, this.state.text.length)
+    return (
+    React.DOM.table(null,
+      React.DOM.thead({
+        onClick: this._sort
+      },
+        React.DOM.tr(null,
+          this.props.headers.map(function(title, idx) {
+              if (this.state.sortby === idx) {
+                  title += this.state.descending ? ' \u2191' : ' \u2193'
+              }
+            return React.DOM.th({ key: idx }, title);
+          }, this)
+        )
+      ),
+      React.DOM.tbody(null,
+        this.state.data.map(function(row, idx) {
+          return (
+          React.DOM.tr({
+            key: idx
+          },
+            row.map(function(cell, idx) {
+              return React.DOM.td({
+                key: idx
+              }, cell);
+            })
+          )
+          );
+        })
+      )
+    )
     );
   }
 });
 
 ReactDOM.render(
-  React.createElement(TextAreaCounter, {
-    text: "ボブ",
+  React.createElement(Excel, {
+    headers: headers,
+    initialData: data,
   }),
   document.getElementById("app")
 );
